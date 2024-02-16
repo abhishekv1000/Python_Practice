b2a643ea159bda3d99bad90bbe9874f2ba9c5f6d
abhishekv1000 <abhishekv1000@outlook.com>
2024-02-16 23:17:11 +0530

    Day 1 Completed

---
+++ Day1.py
@@ -0,0 +1,28 @@
+print("Hellow\nhellow world\n")
+print("Hello " + "+" + "  word")
+print('e.g.print("hel" + "lo")')
+
+print("My Name Is " + input("What Is Your Name ?"))
+ 
+#.....................................................
+
+print("WELCOME TO THE BAND NAME GENERATOR")
+
+a = input("what's name of your city you grown up\n")
+#print(len(a))
+
+b = input("What's Your Pet Name \n")
+
+print("Your Band name could be " + a + " " + b)
+
+#.....................................................
+
+a = input("VALUE OF A\n")
+b = input("VALUE OF B\n")
+
+c =a
+a=b
+b=c
+
+print(a + " " + b)
+
