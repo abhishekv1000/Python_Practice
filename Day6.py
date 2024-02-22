eee760e33af5b20896729bb98b21cd718a1e13fb
abhishekv1000 <abhishekv1000@outlook.com>
2024-02-22 23:11:36 +0530

    Day 6  Python Practice

---
+++ Day6.py
@@ -0,0 +1,27 @@
+# def my_fun():
+#  print("Hello Word")
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+  
+# def jump() :
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+  
+# for i in range (1,7):
+#   jump()
+
+# no = 6
+# while no > 0:
+#     jump()
+#     no -= 1
+
